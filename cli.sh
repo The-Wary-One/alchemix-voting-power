b767ed7439c713e8a5c4dafdaaf7e394d94b69f1
Wary <one.should.be.wary@gmail.com>
2025-01-20 00:07:36 +0100

    🚧 augment the cli; the CI uses it

--- cli.sh
+++ cli.sh
@@ -8,17 +8,17 @@ if [[ "${TRACE-0}" == "1" ]]; then
     set -o xtrace # Enable debug mode when called with `TRACE=1`.
 fi
 
-cd "$(dirname "$0")" # Change to the directory containing this script.
-
 ### UTILITIES ###
 function fmttxt {
     # ANSI text formatting: https://misc.flogisoft.com/bash/tip_colors_and_formatting.
+    # E.g. to apply the 'bold' formatting to the 'Hello' text, we can output '\x1B[1mHello\x1B[22m' to the terminal.
+    # E.g. to apply the 'bold' and 'underlined' formatting to the 'Hello' text, we can output '\x1B[1;4mHello\x1B[22;24m' to the terminal.
 
     local -a formatting_codes_start_arr=() # Create a local -to the function- indexed array.
     local -a formatting_codes_end_arr=()
 
-    while [[ "$1" =~ ^--?[a-z]+$ ]]; do # Loop over the args like '-f' or '--flag'.
-        case "$1" in
+    while [[ "${1}" =~ ^--?[a-z]+$ ]]; do # Loop over the args like '-f' or '--flag'.
+        case "${1}" in
         --bold)
             formatting_codes_start_arr+=('1')
             formatting_codes_end_arr+=('22')
@@ -27,50 +27,110 @@ function fmttxt {
             formatting_codes_start_arr+=('4')
             formatting_codes_end_arr+=('24')
             ;;
+        --red)
+            formatting_codes_start_arr+=('31')
+            formatting_codes_end_arr+=('39')
+            ;;
         *)
-            echo 'Invalid flag.' >&2
+            printf 'Invalid flag.\n' 1>&2
             exit 1
             ;;
         esac
         shift
     done
 
-    local -r remaining_txt="$*" # Join the remaining args with the default '$IFS' separator (i.e. ' ') as a local readonly string.
-    local -r IFS=";"            # Temporarly override the default IFS string separator with the ANSI separator (i.e. ';').
-    echo -e "\x1B[${formatting_codes_start_arr[*]}m$remaining_txt\x1B[${formatting_codes_end_arr[*]}m"
+    local -r remaining_txt="${*}" # Join the remaining args with the default '$IFS' separator (i.e. ' ') as a local readonly string.
+    local -r IFS=";"              # Temporarly override the default IFS string separator with the ANSI separator (i.e. ';').
+    printf "\x1B[%sm%s\x1B[%sm\n" "${formatting_codes_start_arr[*]}" "${remaining_txt}" "${formatting_codes_end_arr[*]}"
 }
 
 ### COMMANDS ###
-function testcommand {
-    forge test \
-        --fork-url "${RPC_MAINNET}" \
-        --fork-block-number "${BLOCK_NUMBER_MAINNET}"
-}
-
-### MAIN SCRIPT ###
-function main {
-    if [[ -e ./.env ]]; then # Source .env only if it exists.
-        source .env          # Add all global variables and functions to the current shell.
-    fi
-
-    case "${1-}" in
-    test | t)
-        shift "$#" # Remove all remaining arguments.
-        testcommand
-        ;;
-
-    *)
-        echo "This is a helper cli.
+function _usage {
+    # Use 'heredoc' into 'cat' to show the usage message.
+    cat <<EOF
+This is a helper cli.
 
 $(fmttxt --bold --underlined 'Usage:') $(fmttxt --bold './cli.sh') <COMMAND>
 
 $(fmttxt --bold --underlined 'Commands:')
     $(fmttxt --bold 'help')         Print this message
     $(fmttxt --bold 'test')         Run the project's tests [aliases: t]
-"
+
+EOF
+}
+
+function _build {
+    forge --version
+    forge build --sizes
+}
+
+function _check {
+    shellcheck --external-sources ./*.sh script/*.sh # Lint the sh scripts.
+    forge fmt --check                                # Check the contracts formatting.
+}
+
+function _test {
+    forge test \
+        --fork-url "${RPC_MAINNET}" \
+        --fork-block-number "${BLOCK_NUMBER_MAINNET}"
+}
+
+### MAIN SCRIPT ###
+# shellcheck disable=SC2317 # Disable the false positive unreachable code.
+function cleanup {
+    local -ir exit_code=${?} # When exitting the script, store the exit code as a local -to the function- readonly integer.
+
+    # Cleanup code here.
+    if [[ ${exit_code} -gt 0 ]]; then                                                            # If there is an error.
+        printf '%s\n' "$(fmttxt --red "Something went wrong during the script execution.")" 1>&2 # Print the error message to stderr.
+    fi
+
+    exit "${exit_code}"
+}
+
+trap cleanup EXIT ERR # Exit trap to make the script more robust (e.g. similar to a try/catch/finally to run some cleanup code).
+
+cd "$(dirname "${0}")" # Change to the directory containing this script.
+
+function main {
+    if [[ ${#} == 0 ]]; then # Print usage message if no args are provided.
+        _usage
         exit 0
+    fi
+
+    local -r command="${1}"
+    shift
+
+    case "$command" in
+    -h | --help | h | help)
+        shift "${#}"
+        _usage
+        ;;
+    build)
+        shift "${#}"
+        _build
+        ;;
+    check)
+        shift "${#}"
+        _check
+        ;;
+    t | test)
+        shift "${#}" # Remove all remaining arguments.
+
+        if [[ -e ./.env ]]; then # Source '.env' only if it exists.
+            source .env          # Add all global variables and functions to the current shell.
+        fi
+
+        _test
+        ;;
+    *)
+        shift "${#}"
+        printf '%s\n' "$(fmttxt --red "Invalid command.")" 1>&2
+        _usage
+        exit 1
         ;;
     esac
 }
 
-main "$@"
+main "${@}"
+exit 0
--- script/generate-deployment-file-local.sh
+++ script/generate-deployment-file-local.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/env bash
+
 set -e
 
 source .env
 
-abi=$(jq -c "{abi: .abi}" ./out/AlchemixVotingPowerCalculator.sol/AlchemixVotingPowerCalculator.json)
+abi=$(jq --compact-output '{abi: .abi}' ./out/AlchemixVotingPowerCalculator.sol/AlchemixVotingPowerCalculator.json)
+
 address=$(jq -c \
-    "{address: .transactions[0].contractAddress}" \
+    '{address: .transactions[0].contractAddress}' \
     ./broadcast/AlchemixVotingPowerCalculatorDeployer.s.sol/31337/run-latest.json)
-blocknumberhex=$(jq -rc \
-    ".receipts[0].blockNumber" \
+
+blocknumberhex=$(jq --raw-output --compact-output \
+    '.receipts[0].blockNumber' \
     ./broadcast/AlchemixVotingPowerCalculatorDeployer.s.sol/31337/run-latest.json)
-blocknumber=$(cast --to-base $blocknumberhex 10)
-echo "$abi $address {\"blockNumber\": $blocknumber}" | jq -s add > ./deployments/31337/AlchemixVotingPowerCalculator.json
+
+blocknumber=$(cast --to-base "${blocknumberhex}" 10)
+
+printf '%s %s {"blockNumber": %i}\n' "${abi}" "${address}" "${blocknumber}" | jq --slurp add >./deployments/31337/AlchemixVotingPowerCalculator.json
