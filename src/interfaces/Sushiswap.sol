d094da8699486ffbf6db5a59f660f397ababaf62
Wary <one.should.be.wary@gmail.com>
2025-02-08 19:17:16 +0100

    ⬆ interfaces solidity version

--- src/interfaces/Sushiswap.sol
+++ src/interfaces/Sushiswap.sol
@@ -1,5 +1,5 @@
 //SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.19;
+pragma solidity ^0.8.28;
 
 interface IUniswapV2Pair {
     function balanceOf(address account) external view returns (uint256);
